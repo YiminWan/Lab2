93b0e0e5ddff7b2e5292c668a59c6090b905ef85
wanyimin <yiminw2@illinois.edu>
2018-01-31 16:32:36 -0600

    done

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,12 +35,20 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
-        }
+            while (maze.isFinished() == false) {
+                maze.turnRight();
+                while (maze.canMove() == false) {
+                    maze.turnLeft();
+                }
+                maze.move();
 
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
-        } else {
-            System.out.println("Try again!");
+            }
+
+
+            if (maze.isFinished()) {
+                System.out.println("You solved the maze!");
+            } else {
+                System.out.println("Try again!");
+            }
         }
-    }
-}
+    }}
